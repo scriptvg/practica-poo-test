ff9d5f248b658dc31a40ba3c2137aa2a7bd692cf
scriptvg <49818250+scriptvg@users.noreply.github.com>
2025-04-16 13:31:20 -0600

    a

--- matematicas.py
+++ matematicas.py
@@ -7,3 +7,13 @@ def es_primo(n):
             return False
     return True
 
+def factorial(n):
+    if n < 0:
+        raise ValueError("El número debe ser no negativo")
+    if n == 0 or n == 1:
+        return 1
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
+
--- test_matemeticas.py
+++ test_matemeticas.py
@@ -1,4 +1,5 @@
-from matematicas import es_primo
+from matematicas import es_primo, factorial
+import pytest
 
 def test_esprimo():
     assert es_primo(2) == True
@@ -7,3 +8,11 @@ def test_esprimo():
     assert es_primo(13) is True
     assert es_primo(1) is False
     assert es_primo(0) is False
+    
+def test_factorial():
+    assert factorial(0) == 1
+    assert factorial(5) == 120
+    with pytest.raises(ValueError):
+        factorial(-3)
+    assert factorial(1) == 1
+
